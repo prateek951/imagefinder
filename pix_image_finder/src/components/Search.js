c3fddde03c8d2790ad4b489bdd83f3ba4a433693
prateek951 <prince.cenation@gmail.com>
2018-07-11 17:05:04 +0530

    images fetched from api

--- pix_image_finder/src/components/Search.js
+++ pix_image_finder/src/components/Search.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 class Search extends PureComponent {
     state = {
@@ -11,7 +12,41 @@ class Search extends PureComponent {
         apiKey: '9522425-eea0dacafe21dcfd3c27cf331',
         images: []
     }
+    onTextChange = e => {
+        this.setState({[e.target.name]: e.target.value},() => {
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=phone&per_page=${this.state.amount}&safesearch=true`)
+            .then(response => {
+                console.log(response.data.hits);
+                this.setState({
+                    images: response.data.hits
+                });
+            }).catch(err => console.log(`An error occured while fetching the images from the api`));
+        })
+    
+    }
+    renderMenuItems(){
+        let items = [
+        {
+            limit: 5,
+            placeholder: '5'
+        },
+        {
+            limit: 10,
+            placeholder: '10'
+        },
+        {
+            limit: 15,
+            placeholder: '15'
+        },
+        {
+            limit: 20,
+            placeholder: '20'
+        }
+    ]
+    return items.map(item => <MenuItem value={item.limit} primaryText={item.placeholder}/>)
+    }
     render() {
+        // console.log(this.state.images);
         return (
             <div>
                 <TextField
@@ -28,11 +63,7 @@ class Search extends PureComponent {
                     value={this.state.amount}
                     onChange={this.onAmountChange}
                     >
-                    <MenuItem value={5} primaryText="5" />
-                    <MenuItem value={10} primaryText="10" />
-                    <MenuItem value={15} primaryText="15" />
-                    <MenuItem value={20} primaryText="20" />
-                    <MenuItem value={25} primaryText="25" />
+                    {this.renderMenuItems()}
                 </SelectField>
             </div>
 
